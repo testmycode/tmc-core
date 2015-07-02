04088f017d8d442fbc1a793105f4887b63924b91
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2015-07-02 16:28:00 +0300

    merge

--- src/main/java/hy/tmc/core/commands/DownloadExercises.java
+++ src/main/java/hy/tmc/core/commands/DownloadExercises.java
@@ -16,7 +16,6 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.charset.Charset;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import org.apache.commons.io.FileUtils;
 import java.util.List;

47c9d42db52037f2b258685475416e50421afcd7
Pihla Toivanen <toivanenpihla@gmail.com>
2015-07-16 10:02:37 +0300

    test skip

--- before_install.sh
+++ before_install.sh
@@ -2,4 +2,4 @@
 # Installs tmc-core
 
 git clone https://github.com/rage/tmc-langs.git
-mvn clean install -U -Dmaven.test.skip=true tmc-langs/pom.xml
+mvn clean install -U -Dmaven.test.skip=true -f tmc-langs/pom.xml

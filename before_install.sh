031f3a1176949c8057975476030c8a896dd56806
Jarmo Isotalo <jamo@isotalo.fi>
2015-08-12 23:07:39 +0300

    Improve documentation of the before_install-script

--- before_install.sh
+++ before_install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-# Installs tmc-core
+# NOTE: should only be used in travis (or other temporary automated setups)
+#
+# Clones and install tmc-langs locally, till the tmc-langs has been officially 
+# released to a public maven repository.
 
 git clone https://github.com/rage/tmc-langs.git
 mvn clean install -U -Dmaven.test.skip=true -f tmc-langs/pom.xml

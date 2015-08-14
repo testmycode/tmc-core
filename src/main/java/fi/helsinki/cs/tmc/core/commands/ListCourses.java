7831e84d20434aa7b3266d60307b51c364c0aefb
ljleppan <leo.leppanen@helsinki.fi>
2015-08-14 15:36:56 +0300

    Refactored commands.ListCourses
    - Added Javadoc
    - Simplified constructors
    - Cleaned up tests

--- src/main/java/fi/helsinki/cs/tmc/core/commands/ListCourses.java
+++ src/main/java/fi/helsinki/cs/tmc/core/commands/ListCourses.java
@@ -1,7 +1,6 @@
 package fi.helsinki.cs.tmc.core.commands;
 
 import fi.helsinki.cs.tmc.core.communication.TmcApi;
-import fi.helsinki.cs.tmc.core.communication.UrlCommunicator;
 import fi.helsinki.cs.tmc.core.configuration.TmcSettings;
 import fi.helsinki.cs.tmc.core.domain.Course;
 import fi.helsinki.cs.tmc.core.exceptions.TmcCoreException;
@@ -9,30 +8,42 @@ import fi.helsinki.cs.tmc.core.exceptions.TmcCoreException;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * A {@link Command} for retrieving the course list from the server.
+ */
 public class ListCourses extends Command<List<Course>> {
 
     private TmcApi tmcApi;
 
+    /**
+     * Constructs a new list courses command with {@code settings}.
+     */
     public ListCourses(TmcSettings settings) {
-        super(settings);
-        this.tmcApi = new TmcApi(settings);
+        this(settings, new TmcApi(settings));
     }
 
+    /**
+     * Constructs a new list courses command with {@code settings} that uses {@code tmcApi} to
+     * communicate with the server.
+     */
     public ListCourses(TmcSettings settings, TmcApi tmcApi) {
         super(settings);
         this.tmcApi = tmcApi;
     }
 
-    public ListCourses(TmcSettings settings, UrlCommunicator communicator) {
-        super(settings);
-        this.tmcApi = new TmcApi(communicator, settings);
-    }
-
+    /**
+     * Entry point for launching this command.
+     */
     @Override
-    public List<Course> call() throws TmcCoreException, IOException {
+    public List<Course> call() throws TmcCoreException {
         if (!settings.userDataExists()) {
             throw new TmcCoreException("User must be authorized first");
         }
-        return tmcApi.getCourses();
+
+        try {
+            return tmcApi.getCourses();
+        } catch (IOException ex) {
+            throw new TmcCoreException("Failed to fetch courses from server", ex);
+        }
     }
 }
--- src/test/java/fi/helsinki/cs/tmc/core/commands/ListCoursesTest.java
+++ src/test/java/fi/helsinki/cs/tmc/core/commands/ListCoursesTest.java
@@ -1,15 +1,14 @@
 package fi.helsinki.cs.tmc.core.commands;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 
 import static org.junit.Assert.assertEquals;
 
 import fi.helsinki.cs.tmc.core.CoreTestSettings;
-import fi.helsinki.cs.tmc.core.TmcCore;
 import fi.helsinki.cs.tmc.core.communication.HttpResult;
+import fi.helsinki.cs.tmc.core.communication.TmcApi;
 import fi.helsinki.cs.tmc.core.communication.UrlCommunicator;
-import fi.helsinki.cs.tmc.core.communication.UrlHelper;
 import fi.helsinki.cs.tmc.core.domain.Course;
 import fi.helsinki.cs.tmc.core.exceptions.TmcCoreException;
 import fi.helsinki.cs.tmc.core.exceptions.TmcServerException;
@@ -17,8 +16,6 @@ import fi.helsinki.cs.tmc.core.testhelpers.ExampleJson;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
-
-import com.google.common.util.concurrent.ListenableFuture;
 
 import org.hamcrest.core.IsInstanceOf;
 
@@ -31,66 +28,55 @@ import org.mockito.Mockito;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 public class ListCoursesTest {
 
-    private ListCourses list;
     private CoreTestSettings settings = new CoreTestSettings();
     private UrlCommunicator communicator;
+    private TmcApi tmcApi;
 
     @Rule public ExpectedException expectedException = ExpectedException.none();
 
-    @Before
-    public void setUp() throws IOException, TmcCoreException {
-        communicator = Mockito.mock(UrlCommunicator.class);
+    @Rule public WireMockRule wireMock = new WireMockRule();
 
-        settings.setUsername("mockattu");
-        settings.setPassword("ei tarvi");
+    @Before
+    public void setUp() throws IOException {
+        communicator = Mockito.mock(UrlCommunicator.class);
+        tmcApi = new TmcApi(communicator, settings);
+
+        settings.setUsername("username");
+        settings.setPassword("password");
+
         HttpResult fakeResult = new HttpResult(ExampleJson.allCoursesExample, 200, true);
         Mockito.when(communicator.makeGetRequestWithAuthentication(Mockito.anyString()))
                 .thenReturn(fakeResult);
-
-        list = new ListCourses(settings, communicator);
-    }
-
-    @Test
-    public void testCheckDataSuccess() throws TmcCoreException, IOException {
-        ListCourses ls = new ListCourses(settings, communicator);
-        settings.setUsername("asdf");
-        settings.setPassword("bsdf");
-        ls.call();
     }
 
     @Test(expected = TmcCoreException.class)
-    public void testNoAuthThrowsException() throws TmcCoreException, Exception {
-        settings.setUsername("");
-        settings.setPassword("");
-        list.call();
+    public void testNoAuthThrowsException() throws TmcCoreException {
+        settings.setUsername(null);
+        settings.setPassword(null);
+        new ListCourses(settings, tmcApi).call();
     }
 
     @Test
     public void testWithAuthSuccess() throws Exception {
-        List<Course> courses = list.call();
+        List<Course> courses = new ListCourses(settings, tmcApi).call();
         assertEquals("2013_ohpeJaOhja", courses.get(0).getName());
     }
 
-    @Rule public WireMockRule wireMock = new WireMockRule();
-
     @Test
-    public void listCoursesWillThrowExceptionIfAuthFailedOnServer()
-            throws ExecutionException, InterruptedException, TmcCoreException {
+    public void listCoursesWillThrowExceptionIfAuthFailedOnServer() throws TmcCoreException {
         expectedException.expectCause(IsInstanceOf.<Throwable>instanceOf(TmcServerException.class));
         wireMock.stubFor(
-                get(urlEqualTo(new UrlHelper(settings).withParams("/courses.json")))
+                get(urlMatching(".*"))
                         .willReturn(WireMock.aResponse().withStatus(401)));
 
         CoreTestSettings localSettings = new CoreTestSettings();
-        localSettings.setUsername("testy");
-        localSettings.setPassword("1234");
+        localSettings.setUsername("username");
+        localSettings.setPassword("password");
         localSettings.setServerAddress("http://localhost:8080");
-        TmcCore core = new TmcCore(localSettings);
-        ListenableFuture<List<Course>> courses = core.listCourses();
-        courses.get();
+
+        new ListCourses(localSettings, new TmcApi(localSettings)).call();
     }
 }

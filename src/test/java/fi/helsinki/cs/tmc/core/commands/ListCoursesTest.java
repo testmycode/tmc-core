fc047d57f9ab51e44041fd62e0ce9d2aa2d19b8f
Pihla Toivanen <toivanenpihla@gmail.com>
2015-08-25 17:47:34 +0300

    test

--- src/test/java/fi/helsinki/cs/tmc/core/commands/ListCoursesTest.java
+++ src/test/java/fi/helsinki/cs/tmc/core/commands/ListCoursesTest.java
@@ -66,24 +66,9 @@ public class ListCoursesTest {
         List<Course> courses = new ListCourses(settings, tmcApi).call();
         assertEquals("2013_ohpeJaOhja", courses.get(0).getName());
     }
-
-    /*@Test
-    public void listCoursesWillThrowExceptionIfAuthFailedOnServer() throws TmcCoreException {
-        expectedException.expectCause(IsInstanceOf.<Throwable>instanceOf(TmcServerException.class));
-        wireMock.stubFor(
-                get(WireMock.urlPathEqualTo("/courses.json"))
-                        .willReturn(WireMock.aResponse().withStatus(401)));
-
-        CoreTestSettings localSettings = new CoreTestSettings();
-        localSettings.setUsername("username");
-        localSettings.setPassword("password");
-        localSettings.setServerAddress("http://localhost:8080");
-
-        new ListCourses(localSettings, new TmcApi(localSettings)).call();
-    }*/
     
+    @Test(expected=TmcCoreException.class)
     public void listCoursesWillThrowExceptionIfAuthFailedOnServer() throws TmcCoreException {
-        expectedException.expectCause(IsInstanceOf.<Throwable>instanceOf(TmcCoreException.class));
         wireMock.stubFor(
                 get(WireMock.urlPathEqualTo("/courses.json"))
                         .willReturn(WireMock.aResponse().withStatus(401)));

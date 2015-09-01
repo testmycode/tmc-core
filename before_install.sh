681032b2feca995201b030c7a068f5549f1887c0
Jarmo Isotalo <jamo@isotalo.fi>
2015-09-01 20:14:10 +0300

    Install langs from correct source and inline the before install script

--- before_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# NOTE: should only be used in travis (or other temporary automated setups)
-#
-# Clones and install tmc-langs locally, till the tmc-langs has been officially 
-# released to a public maven repository.
-
-git clone https://github.com/rage/tmc-langs.git
-mvn clean install -U -Dmaven.test.skip=true -f tmc-langs/pom.xml

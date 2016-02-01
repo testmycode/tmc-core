7ce2f6ddceb65ddd90e39ebc0ff5e3e08eadbd2c
ljleppan <leo.leppanen@helsinki.fi>
2016-02-01 17:40:40 +0200

    Replace check with preconditions

--- src/main/java/fi/helsinki/cs/tmc/core/util/ParameterTester.java
+++ src/main/java/fi/helsinki/cs/tmc/core/util/ParameterTester.java
@@ -4,13 +4,13 @@ import static com.google.common.base.Strings.isNullOrEmpty;
 
 import fi.helsinki.cs.tmc.core.exceptions.TmcCoreException;
 
+import com.google.common.base.Preconditions;
+
 public class ParameterTester {
 
     public static void checkStringParameters(String... params) throws TmcCoreException {
         for (String param : params) {
-            if (isNullOrEmpty(param)) {
-                throw new TmcCoreException("Param {" + param + "}empty or null.");
-            }
+            Preconditions.checkArgument(!isNullOrEmpty(param), "Param {" + param + "}empty or null.");
         }
     }
 }

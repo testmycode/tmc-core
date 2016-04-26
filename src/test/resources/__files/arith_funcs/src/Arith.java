d80577348d9e4fed9d35e5d67d17b9b29d80fc74
Jarmo Isotalo <jamo@isotalo.fi>
2016-04-26 16:04:39 +0300

    TestResources

---
+++ src/test/resources/__files/arith_funcs/src/Arith.java
@@ -0,0 +1,17 @@
+public class Arith {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+
+    public static int div(int a, int b) {
+        return a / b;
+    }
+}

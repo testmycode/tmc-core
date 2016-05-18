1f4888bbd45d4b93f86382d8b2bd44b4bac7db17
Jarmo Isotalo <jamo@isotalo.fi>
2016-05-18 08:24:24 +0300

    Fix autodeployment

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-mvn deploy deploy.xml
+if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" -a "$TRAVIS_JDK_VERSION" = "oraclejdk8" ]; then
+  echo "Deploying"
+  mvn deploy --settings deploy.xml
+else
+  echo "Not on master or PR or java8 -> skipping deploy"
+fi

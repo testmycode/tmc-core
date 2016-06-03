f8676d4581f95b196870ff2fddb910953e94a53e
Jarmo Isotalo <jamo@isotalo.fi>
2016-06-03 13:21:08 +0300

    Flaky tests

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" -a "$TRAVIS_JDK_VERSION" = "oraclejdk8" ]; then
   echo "Deploying"
-  mvn deploy -Dsurefire.rerunFailingTestsCount=3 --settings deploy.xml
+  # We already ran the tests
+  mvn deploy -Dmaven.test.skip=true --settings deploy.xml
 else
   echo "Not on master or PR or java8 -> skipping deploy"
 fi

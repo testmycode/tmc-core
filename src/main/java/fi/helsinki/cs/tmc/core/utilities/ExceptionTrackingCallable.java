cedfb5a89b2d722a7aeb1deb12e476659493c4f6
Antti Leinonen <antti.leinonen@helsinki.fi>
2017-03-15 14:39:54 +0200

    Don't send Crash data if diagnostics is disabled

--- src/main/java/fi/helsinki/cs/tmc/core/utilities/ExceptionTrackingCallable.java
+++ src/main/java/fi/helsinki/cs/tmc/core/utilities/ExceptionTrackingCallable.java
@@ -1,7 +1,9 @@
 package fi.helsinki.cs.tmc.core.utilities;
 
 import fi.helsinki.cs.tmc.core.communication.TmcBandicootCommunicationTaskFactory;
+import fi.helsinki.cs.tmc.core.configuration.TmcSettings;
 import fi.helsinki.cs.tmc.core.domain.bandicoot.Crash;
+import fi.helsinki.cs.tmc.core.holders.TmcSettingsHolder;
 
 import com.google.common.annotations.VisibleForTesting;
 
@@ -11,16 +13,19 @@ public class ExceptionTrackingCallable<T> implements Callable<T> {
 
     private final Callable<T> command;
     private final TmcBandicootCommunicationTaskFactory tmcBandicootCommunicationTaskFactory;
+    private final TmcSettings settings;
 
     public ExceptionTrackingCallable(final Callable<T> command) {
         this.command = command;
         this.tmcBandicootCommunicationTaskFactory = new TmcBandicootCommunicationTaskFactory();
+        this.settings = TmcSettingsHolder.get();
     }
 
     @VisibleForTesting
     public ExceptionTrackingCallable(final Callable<T> command, TmcBandicootCommunicationTaskFactory factory) {
         this.command = command;
         this.tmcBandicootCommunicationTaskFactory = factory;
+        this.settings = TmcSettingsHolder.get();
     }
 
     @Override
@@ -28,7 +33,9 @@ public class ExceptionTrackingCallable<T> implements Callable<T> {
         try {
             return command.call();
         } catch (Exception ex) {
-            tmcBandicootCommunicationTaskFactory.sendCrash(new Crash(ex)).call();
+            if (settings.getSendDiagnostics()) {
+                tmcBandicootCommunicationTaskFactory.sendCrash(new Crash(ex)).call();
+            }
             throw ex;
         }
     }
